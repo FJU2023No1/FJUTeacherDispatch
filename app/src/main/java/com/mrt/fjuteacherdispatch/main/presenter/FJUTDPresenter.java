db1da7f8e3158aa1ca972ffa32443ed01b600a19
林裕淵 <>
2023-10-28 15:51:57 +0800

    新增 區分 使用者身份 Button

--- app/src/main/java/com/mrt/fjuteacherdispatch/main/presenter/FJUTDPresenter.java
+++ app/src/main/java/com/mrt/fjuteacherdispatch/main/presenter/FJUTDPresenter.java
@@ -1,6 +1,7 @@
 package com.mrt.fjuteacherdispatch.main.presenter;
 
 import android.content.Context;
+import android.widget.Toast;
 
 import com.mrt.fjuteacherdispatch.main.model.FJUTDModel;
 import com.mrt.fjuteacherdispatch.main.view.FJUTDActivity;
@@ -19,5 +20,17 @@ public class FJUTDPresenter {
 
     public void init() {
 
+    }
+
+    public void onGoToStudent() {
+        Toast.makeText(mActivity, "Student", Toast.LENGTH_LONG).show();
+    }
+
+    public void onGoToTeacher() {
+        Toast.makeText(mActivity, "Teacher", Toast.LENGTH_LONG).show();
+    }
+
+    public void onGoToAdministrator() {
+        Toast.makeText(mActivity, "Admin", Toast.LENGTH_LONG).show();
     }
 }
