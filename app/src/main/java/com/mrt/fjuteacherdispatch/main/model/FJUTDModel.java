18e0cdbe5697311cac7de2b6a9b02ae1a4bb6419
林裕淵 <>
2023-10-30 19:51:14 +0800

    調整 首頁UI

--- app/src/main/java/com/mrt/fjuteacherdispatch/main/model/FJUTDModel.java
+++ app/src/main/java/com/mrt/fjuteacherdispatch/main/model/FJUTDModel.java
@@ -1,4 +1,13 @@
 package com.mrt.fjuteacherdispatch.main.model;
 
+import androidx.databinding.ObservableField;
+
 public class FJUTDModel {
+
+    /**
+     * 身份.
+     * 學生，老師，管理者.
+     */
+    public final ObservableField<String> userIdentityText = new ObservableField<>();
+
 }
